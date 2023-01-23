e8fc08fec53672afd9138c8f27da915dad8cbf0a
Fawaz Oduola <oduolafawaz@gmail.com>
2023-01-23 21:27:09 +0100

    done

---
+++ 1-simple_pagination.py
@@ -0,0 +1,36 @@
+import csv
+import math
+from typing import List
+
+def index_range(page, page_size):
+    start_index =  (page-1)*page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:10]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            assert type(page) is int and page > 0
+            assert type(page_size) is int and page_size > 0
+            try:
+                ranges = index_range(page,page_size)
+                return self.dataset()[ranges[0]:ranges[1]]
+            except IndexError:
+                 return []
