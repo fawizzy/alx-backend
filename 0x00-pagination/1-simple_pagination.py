c7619bfcfab56fc979767251b91d774c60aae811
Fawaz Oduola <oduolafawaz@gmail.com>
2023-01-24 22:26:35 +0100

    done

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -5,6 +5,10 @@ from typing import List
 
 
 def index_range(page, page_size):
+    """
+    returns a tuple of size two containing a start index
+    and end index corresponding to the range of indexes to return
+    """
     start_index = (page-1)*page_size
     end_index = start_index + page_size
     return (start_index, end_index)
@@ -30,6 +34,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        return content of the page
+        """
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
         try:
