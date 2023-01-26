8fc75ff23e1151825067eb05c87dcbec693e11d8
Fawaz Oduola <oduolafawaz@gmail.com>
2023-01-26 06:50:22 +0100

    done

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -21,11 +21,12 @@ class FIFOCache(BaseCaching):
         """
         if key is None or item is None:
             return
-        self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+        
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             del_key = list(self.cache_data.keys())[0]
             del self.cache_data[del_key]
             print("DISCARD: ", del_key)
+        self.cache_data[key] = item
 
     def get(self, key):
         """
