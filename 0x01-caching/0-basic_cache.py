0a17879d0d12b3ee0c383d08a129477ddca82cf9
Fawaz Oduola <oduolafawaz@gmail.com>
2023-01-26 06:28:47 +0100

    done

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -20,6 +20,12 @@ class BasicCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
+        """
+        Store a key-value pair
+        Args:
+            Key
+            Item
+        """
         if key is None:
             return
         self.cache_data[key] = item
