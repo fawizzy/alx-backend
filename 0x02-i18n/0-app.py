e1f15769c6310aaf9dcb423a99296b1a5beafc8d
Fawaz Oduola <oduolafawaz@gmail.com>
2023-04-26 10:58:52 +0100

    done

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,3 +11,7 @@ def index():
     Function to render the 0-index html file
     '''
     return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
