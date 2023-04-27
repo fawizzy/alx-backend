e75d981d687e9667a20ab61cb12f745b4f7d4e26
Fawaz Oduola <oduolafawaz@gmail.com>
2023-04-27 02:20:23 +0100

    1 app added

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config(object):
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    '''
+    Function to render the 0-index html file
+    '''
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
