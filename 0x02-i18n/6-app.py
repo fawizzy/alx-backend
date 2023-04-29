71475958bd4fb5b99bc51d2f36dd5b420603cff2
Fawaz Oduola <oduolafawaz@gmail.com>
2023-04-29 19:30:03 +0100

    task 6 done

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,6 +1,11 @@
 #!/urs/bin/env python3
+'''
+Flask app
+'''
 from flask import Flask, request, render_template, g
 from flask_babel import Babel
+
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -23,7 +28,31 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    '''
+    Select and return best language match based on supported languages
+    '''
+    locale = request.args.get('locale')
+    # check if the locale argument is present
+    if locale is not None:
+        # check if the argument's value is a supported locale
+        if locale in Config.LANGUAGES:
+            return locale
+
+    locale = g.user['locale']
+    if locale in Config.LANGUAGES:
+        return g.user["locale"]
+    locale = request.headers.get('locale', None)
+    if locale in Config.LANGUAGES:
+        return locale
+    return Config.BABEL_DEFAULT_LOCALE
+
+
 def get_user():
+    '''
+    Get user from url
+    '''
     id = request.args.get('login_as', None)
     if id is not None and int(id) in users.keys():
         return users[int(id)]
@@ -32,6 +61,9 @@ def get_user():
 
 @app.before_request
 def before_request():
+    '''
+    get user from url and set as global user
+    '''
     user = get_user()
     g.user = user
 
@@ -41,7 +73,7 @@ def index():
     '''
     render template
     '''
-    return render_template('5-index.html')
+    return render_template('6-index.html')
 
 
 if __name__ == "__main__":
