82f43de68d8f0c3adf9b34ebc396976b47307d19
Fawaz Oduola <oduolafawaz@gmail.com>
2023-05-20 08:19:15 +0100

    done

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,41 +1,25 @@
-#!/usr/bin/env python3
-"""
-    Fifo cache
-"""
 from base_caching import BaseCaching
 
-
 class FIFOCache(BaseCaching):
-    """
-        FIFO caching system
-    """
     def __init__(self):
-        """
-        initialize class parent's init method
-        """
         super().__init__()
 
     def put(self, key, item):
-        """
-            Cache a key value
-        """
         if key is None or item is None:
             return
 
-        if len(self.cache_data) >= self.MAX_ITEMS and key not in self.cache_data:
-            first_in = list(self.cache_data.keys())[0]
-            del self.cache_data[first_in]
-            print("DISCARD: ", first_in)
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            # Get the first item (oldest) in the cache
+            first_item = next(iter(self.cache_data))
+            # Remove the first item from the cache
+            del self.cache_data[first_item]
+            print("DISCARD:", first_item)
+
+        # Assign the item value to the key in the cache
         self.cache_data[key] = item
 
     def get(self, key):
-        """
-            Return the value of the key from the dictionary
-        """
-        if key is None:
+        if key is None or key not in self.cache_data:
             return None
 
-        if key in self.cache_data.keys():
-            return self.cache_data[key]
-        else:
-            return None
+        return self.cache_data[key]
