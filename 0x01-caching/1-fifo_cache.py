6f2e131872eb7301f439cd228121f0b3f876fd83
Fawaz Oduola <oduolafawaz@gmail.com>
2023-05-20 08:21:02 +0100

    done

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,10 +1,38 @@
 from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
+    """
+    FIFOCache is a caching system that inherits from BaseCaching class.
+    It implements a caching system using the First-In-First-Out (FIFO) algorithm.
+    """
+
     def __init__(self):
+        """
+        Initializes an instance of FIFOCache.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         super().__init__()
 
     def put(self, key, item):
+        """
+        Inserts an item into the cache with the specified key.
+
+        If the key or item is None, this method does nothing.
+        If the number of items in the cache exceeds the maximum limit (BaseCaching.MAX_ITEMS),
+        it discards the first item put in the cache based on the FIFO algorithm.
+
+        Args:
+            key: The key to associate the item with.
+            item: The item to be stored in the cache.
+
+        Returns:
+            None
+        """
         if key is None or item is None:
             return
 
@@ -19,6 +47,17 @@ class FIFOCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
+        """
+        Retrieves the value associated with the specified key from the cache.
+
+        If the key is None or does not exist in the cache, it returns None.
+
+        Args:
+            key: The key to retrieve the value for.
+
+        Returns:
+            The value associated with the key in the cache, or None if the key is None or not found.
+        """
         if key is None or key not in self.cache_data:
             return None
 
